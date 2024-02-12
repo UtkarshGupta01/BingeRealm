1a9965da52bb64e1f901d832f31da8326f26f813
utkarsh gupta <ug53054@gmail.com>
2024-02-12 23:51:47 +0530

    updated code

--- src/pages/SearchedResults/SearchedResult.jsx
+++ src/pages/SearchedResults/SearchedResult.jsx
@@ -1,5 +1,66 @@
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useParams } from "react-router-dom";
+import { fetchData } from "../../utils/api";
+import SectionWrapper from "../../components/SectionWrapper/SectionWrapper";
+import Cards from "../../components/Cards/Cards";
+import {
+  SearchFor,
+  Spinner,
+  ResultsFound,
+} from "./styles/SearchedResult.styled";
+import spinner from "/gif/spinner.gif";
+
 const SearchedResult = () => {
-  return <div>SearchedResult</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchData(`/search/multi?query=${query}&page=${pageNum}`).then((res) => {
+      setData(res);
+      setPageNum((prev) => prev + 1);
+      setLoading(false);
+    });
+  };
+
+  const fetchNextDataset = () => {
+    fetchData(`/search/multi?query=${query}&page=${pageNum}`).then((res) => {
+      if (data.results) {
+        setData({
+          ...data,
+          results: [...data.results, ...res.results],
+        });
+      } else {
+        setData(res);
+      }
+      setPageNum((prev) => prev + 1);
+    });
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, [query]);
+
+  return (
+    <SectionWrapper>
+      <SearchFor>
+        Search results for &ldquo;<i>{`${query}`}</i>&rdquo;
+      </SearchFor>
+      {loading && <Spinner src={spinner} />}
+      {!loading && (
+        <ResultsFound>
+          {data.results.length > 0 ? (
+            <></>
+          ) : (
+            <span>Sorry, Results not Found</span>
+          )}
+        </ResultsFound>
+      )}
+    </SectionWrapper>
+  );
 };
 
 export default SearchedResult;
---
+++ src/pages/SearchedResults/styles/SearchedResult.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SearchFor = styled.div`
+padding: 40px 0;
+font-size: 25px;
+font-weight: 500;
+color: white;
+`
+
+export const Spinner = styled.img`
+width: 5%;
+display: block;
+margin: 220px auto 320px;
+`
